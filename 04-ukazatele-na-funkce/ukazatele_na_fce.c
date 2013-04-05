414195f95e1594d31a87d0d53816ef0db07267d2
Eliska Vlckova <efox.vlckova@gmail.com>
2013-04-06 00:10:52 +0200

    pridan ukol-01,02 a 04 fcni ukazatele

---
+++ 04-ukazatele-na-funkce/ukazatele_na_fce.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*Napište v jazyku C funkci double akumulator(double (*fce)(double, double), double cisla[], int pocet),
+která zpracuje pomocí předané funkce fce hodnoty z pole cisla, jehož velikost je dána parametrem pocet.
+Vytvořenou funkci otestujte ve funkci main;
+použitými akumulačními funkcemi mohou být například funkce pro součet nebo součin dvou reálných čísel,
+které je ovšem pro testování potřeba dodefinovat.
+*/
+
+double soucet (double x, double y){
+    return x+y;
+}
+
+double soucin (double x, double y){
+    return x*y;
+}
+
+double akumulator (double (*fce)(double, double), double cisla[], int pocet) {
+    double acc = cisla[0];
+    int i;
+    for (i=1; i<pocet; i++){
+        acc= fce(acc, cisla[i]);
+    }
+    return acc;
+}
+
+double na2 (double x) {
+    return x*x;
+}
+
+double na3 (double x) {
+    return na2(x)*x;
+}
+
+void printna (double (*pf)(double), double x) {
+    printf("pf(%g) = %g\n", x, pf(x));
+}
+
+int main()
+{
+     double p[10];
+     int i;
+
+    for (i=0; i<10; i++)
+      p[i] = i+1;
+
+   printf("Suma je: %g\n", akumulator(soucet,p,10));
+   printf("Produkt je: %g\n", akumulator(soucin,p,10));
+
+    double (*pf) (double);
+    pf = sqrt;
+    printf(" na2 = %p\n", na2);
+    printf("&na2 = %p\n", &na2);
+    printf(" pf = %p\n", pf);
+    printf("&pf = %p\n", &pf);
+    printf(" na2(3) = %g\n", na2(3));
+    printf(" pf(3) = %g\n", pf(3));
+    printna(na3, 5);
+    printna(na2, 4);
+    printna(pf, 4);
+    return 0;
+}
---
+++ ukol-01/relace.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **relace_make(int n){
+char **R;
+int i;
+R = malloc(sizeof(char*)*n);
+for (i=0; i<n; i++) {
+    R[i] = malloc(sizeof(char)*n);
+    }
+return R;
+}
+
+char** relace_copy(char **R, int n){
+char **S;
+int i;
+S = relace_make(n);
+for (i=0; i<n; i++){
+    memcpy(S[i], R[i], sizeof(char)*n);
+    }
+    return S;
+}
+
+void relace_print (char **R, int n) {
+int i, j;
+printf("{");
+for (i=0; i<n; i++){
+    for (j=0; j<n; j++){
+ //     printf("R[%i][%i]=%i\n",i,j,R[i][j]);
+        if ( R[i][j])
+            printf("(%i, %i), ", i, j);
+        }
+    }
+    printf("}\n");
+}
+
+
+char **reflexivni(char **R, int n) {
+char **C;
+int i;
+C = relace_copy(R, n);
+for (i=0; i<n; i++){
+    if (!C[i][i])
+        C[i][i]=  1;
+    }
+    return C;
+}
+
+char **symetricky(char **R, int n) {
+char **C;
+int i, j;
+C = relace_copy(R, n);
+for (i=0; i<n; i++){
+    for (j=0; j<n; j++) {
+    if (C[i][j] && !C[j][i])
+        C[j][i] = 1;
+        }
+    }
+    return C;
+}
+
+char **tranzitivni(char **R, int n) {
+char **C;
+int i, j, k;
+C = relace_copy(R, n);
+for (i=0; i<n; i++){
+    for (j=0; j<n; j++) {
+    if (C[i][j])
+        for (k=0; k<n; k++){
+            if (C[j][k] && !C[i][k])
+            C[i][k] = 1;
+            }
+        }
+    }
+    return C;
+}
+
+
+int main()
+{
+char **A;
+char **B;
+int n = 5;
+char **R, **S, **T;
+A = relace_make(n);
+A[0][1] = A[1][2] = A[2][3]  = 1;
+B = relace_copy(A, n);
+
+printf("relace A je: ");
+relace_print(A, n);
+//relace_print(B, n); kontrola kopie
+printf("reflexivni uzaver relace A je: ");
+R = reflexivni(A, n);
+relace_print(R, n);
+
+printf("symetricky uzaver relace B je: ");
+S = symetricky(A, n);
+relace_print(S, n);
+
+printf("tranzitivni uzaver relace C je: ");
+T = tranzitivni(A, n);
+relace_print(T, n);
+
+    return 0;
+}
