de0baa9b64c94f0d499055e21060fa0a7fd3c2ea
Eliska Vlckova <efox.vlckova@gmail.com>
2013-04-07 19:47:31 +0200

    součty zlomků

---
+++ 08-prace-s-textovymi-soubory/soucty_zlomku/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Napište v jazyku C funkci zlomek soucet(const char *vstup),
+ která čte ze vstupního textového souboru vstup zlomky (resp. dvojice celých čísel),
+  vypočítává a vrací součet všech zlomků zapsaných ve vstupním souboru.
+   Pro snadnější práci se zlomky si definujte strukturovaný typ zlomek.
+   Výsledný zlomek by měl být upraven do základního tvaru.
+   Vykrácení zlomku do základního tvaru docílíte vydělením čitatele i jmenovatele jejich největším společným dělitelem,
+   který můžete určit například použitím Euklidova algoritmu.
+
+   */
+
+int main()
+{
+
+    return 0;
+}
