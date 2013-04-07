14dca51a90ae1bef42b3521544109c0ab6ab8dde
Eliska Vlckova <efox.vlckova@gmail.com>
2013-04-07 18:57:52 +0200

    Fibbonačiho čísla

---
+++ 01-rekurzivni-fce/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    return 0;
+}
+
+{
+    double fib=0, Nmin1, Nmin2, temp;
+    if (n<2) return n;
+    Nmin1=1;
+    Nmin2=0;
+    for (int i=2; i<n; i++)
+    {
+        temp=Nmin1;
+        Nmin1=Nmin1+Nmin2;
+        Nmin2=temp;
+    }
+    return (Nmin1+Nmin2);
+}
