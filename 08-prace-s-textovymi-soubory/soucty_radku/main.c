3b0aa417bbb155159a5c4f6c58305288a1217994
Eliska Vlckova <efox.vlckova@gmail.com>
2013-04-07 18:48:02 +0200

    osma lekce  Please enter the commit message for your changes. Lines starting

---
+++ 08-prace-s-textovymi-soubory/soucty_radku/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Napište v jazyku C funkci int soucty(const char *vstup, const char *vystup),
+která čte ze vstupního souboru vstup desetinná čísla, počítá součty na jednotlivých řádcích a zapisuje je do výstupního souboru vystup.
+Na konec výstupního souboru pak navíc vloží sumu všech čísel ve vstupním souboru.
+//http://jazykc.inf.upol.cz/prace-s-textovymi-soubory/soucty-radku.htm//
+*/
+
+int main()
+{
+
+    return 0;
+}
